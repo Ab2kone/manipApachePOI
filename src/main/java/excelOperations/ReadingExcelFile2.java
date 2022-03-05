2790797b83aaf86e711f845ef5c4c66fdcf436ba
Aboubakar KONE <ab2kone@gmail.com>
2022-03-05 20:06:29 +0000

    Lecteur et affichage du contenu d'un fichier excel en utilisant [ITERATOR]

--- src/main/java/excelOperations/ReadingExcelFile2.java
+++ src/main/java/excelOperations/ReadingExcelFile2.java
@@ -6,10 +6,10 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
-public class ReadingExcelFile {
+public class ReadingExcelFile2 {
 
     public static void main(String[] args) throws IOException {
 
@@ -25,28 +25,31 @@ public class ReadingExcelFile {
 
 
         /**
-         * reccupération de la dernière
-         * ligne et colonne du fichier
+         * Utilisation des itérators pour le lecture
+         * de contenu
          */
-        int rows = sheet.getLastRowNum();
-        int cols = sheet.getRow(1).getLastCellNum();
 
-        for (int r = 0; r <= rows; r++){
+        Iterator iterator = sheet.iterator();
 
-            XSSFRow row = sheet.getRow(r);
+        while (iterator.hasNext()){
+            XSSFRow row = (XSSFRow) iterator.next();
 
-            for (int c = 0; c < cols; c++){
-                XSSFCell cell = row.getCell(c);
-               switch ( cell.getCellType()) {
-                   case STRING: System.out.print(cell.getStringCellValue()); break;
-                   case NUMERIC: System.out.print(cell.getNumericCellValue()); break;
-                   case BOOLEAN: System.out.print(cell.getBooleanCellValue()); break;
-               }
+            Iterator cellIterator = row.cellIterator();
 
+            while (cellIterator.hasNext()){
+                XSSFCell cell = (XSSFCell) cellIterator.next();
+
+                switch ( cell.getCellType()) {
+                    case STRING: System.out.print(cell.getStringCellValue()); break;
+                    case NUMERIC: System.out.print(cell.getNumericCellValue()); break;
+                    case BOOLEAN: System.out.print(cell.getBooleanCellValue()); break;
+                }
                 System.out.print(" | ");
             }
 
             System.out.println();
+
+
         }
 
 
